7e885135681050a086afaf13d1ef8edf4fb2072a
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-05-22 14:48:15 -0500

    setup jawsDb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.DB_PW,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: process.env.DB_PW,
+        database: "burgers_db"
+    });
+}
+
+
+
 
 connection.connect(function(err) {
     if (err) {
